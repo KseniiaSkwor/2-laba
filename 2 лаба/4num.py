3304708ac199dd6abfd6e9e79ff840661bd60364
sser11 <siriusly@bk.ru>
2024-02-26 20:11:19 +0300

    2 laba

---
+++ 2 лаба/4num.py
@@ -0,0 +1,15 @@
+import random
+
+a = ["красный", "синий", "желтый"]
+color1 = random.choice(a)
+color2 = random.choice(a)
+
+print(color1, color2)
+if (color1 == "красный" and color2 == "синий") or (color2 == "красный" and color1 == "синий"):
+    print("фиолетовый")
+elif (color1 == "красный" and color2 == "желтый") or (color2 == "красный" and color1 == "желтый"):
+    print("ораньжевый")
+elif (color1 == "синий" and color2 == "желтый") or (color2 == "синий" and color1 == "желтый"):
+    print("зеленый")
+else:
+    print("Не смешается")
